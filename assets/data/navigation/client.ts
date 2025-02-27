db4aa02c2b3773911628fca22a354e81392ac283
loicmaes <production@maesloic.fr>
2025-02-28 00:21:47 +0100

    :rocket: Setup roles page

--- assets/data/navigation/client.ts
+++ assets/data/navigation/client.ts
@@ -1,4 +1,4 @@
-import { Home } from "lucide-vue-next";
+import { Home, Tags } from "lucide-vue-next";
 import type { INavSection } from "~/types/frontend/navigation";
 
 export const navSections: INavSection[] = [
@@ -9,6 +9,17 @@ export const navSections: INavSection[] = [
         label: "overview",
         icon: Home,
         url: "/",
+      },
+    ],
+  },
+  {
+    label: "administration",
+    children: [
+      {
+        type: "item",
+        label: "roles",
+        icon: Tags,
+        url: "/roles",
       },
     ],
   },
---
+++ pages/app/[companyUuid]/roles.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { IBackCompany } from "~/types/companies/companies";
+
+definePageMeta({
+  layout: "app-default",
+  authRequired: true,
+});
+
+const { t } = useI18n();
+const store = useCompaniesStore();
+const company = computed(() => store.selectedCompany as IBackCompany);
+
+useHead({
+  title: t("tabs.company", {
+    page: t("roles.tab"),
+    name: company.value.name,
+  }),
+});
+</script>
+
+<template>
+  <div role="main">
+    ROLES
+  </div>
+</template>
