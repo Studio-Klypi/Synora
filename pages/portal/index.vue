bdca5796125290bfcb4ada65b6043b64c20bfcef
loicmaes <production@maesloic.fr>
2025-02-27 18:30:35 +0100

    :speech_balloon: Setup literals in lang file

--- pages/portal/index.vue
+++ pages/portal/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { Plus } from "lucide-vue-next";
+import { Plus, LoaderCircle } from "lucide-vue-next";
 import CreateCompanyDialog from "~/components/library/companies/CreateCompanyDialog.vue";
 
 definePageMeta({
   layout: "app-empty",
   authRequired: true,
+});
+
+const { t } = useI18n();
+useHead({
+  title: t("portal.tab", { name: useBrandName() }),
 });
 
 const companiesStore = useCompaniesStore();
@@ -20,45 +25,74 @@ if (!companies.value.length)
     role="main"
     class="flex-1 flex flex-col items-center justify-center gap-4"
   >
-    <nav class="flex flex-wrap gap-2 items-center">
+    <nav class="flex flex-col items-center gap-4">
       <template v-if="loading">
-        loading
+        <div class="w-full grid place-items-center">
+          <LoaderCircle class="animate-spin" />
+        </div>
       </template>
       <template v-else>
         <template v-if="companies.length">
-          <Button
-            v-for="company in companies"
-            :key="company.uuid"
-            variant="outline"
-            class="h-auto p-0 rounded-full"
-            as-child
-          >
-            <NuxtLinkLocale :to="`/app/${company.uuid}/`">
-              <Avatar size="base">
-                <AvatarFallback>{{ company.name.substring(0, 2).toUpperCase() }}</AvatarFallback>
-              </Avatar>
-            </NuxtLinkLocale>
-          </Button>
+          <h2 class="text-lg font-bold mb-2">
+            {{ t("portal.title") }}
+          </h2>
+
+          <div class="flex flex-wrap gap-4 items-center">
+            <Button
+              v-for="company in companies"
+              :key="company.uuid"
+              variant="outline"
+              size="avatar"
+              class="outline outline-border hover:outline-offset-4 transition-all"
+              as-child
+            >
+              <NuxtLinkLocale :to="`/app/${company.uuid}/`">
+                <Avatar size="lg">
+                  <AvatarFallback>{{ company.name.substring(0, 2).toUpperCase() }}</AvatarFallback>
+                </Avatar>
+              </NuxtLinkLocale>
+            </Button>
+
+            <CreateCompanyDialog>
+              <Button
+                variant="outline"
+                size="avatar"
+                class="text-muted-foreground"
+              >
+                <div class="flex">
+                  <Plus />
+                </div>
+              </Button>
+            </CreateCompanyDialog>
+          </div>
         </template>
         <template v-else>
-          no companies
-        </template>
+          <h2 class="text-muted-foreground">
+            {{ t("portal.empty.label") }}
+          </h2>
 
-        <CreateCompanyDialog>
-          <Button
-            variant="secondary"
-            class="p-6 h-auto rounded-full"
-          >
-            <div class="flex">
+          <CreateCompanyDialog>
+            <Button class="mt-2">
               <Plus />
-            </div>
-          </Button>
-        </CreateCompanyDialog>
+              {{ t("portal.empty.action") }}
+            </Button>
+          </CreateCompanyDialog>
+        </template>
       </template>
     </nav>
 
-    <footer>
-      go to administration
-    </footer>
+    <!-- TODO: enable admin panel -->
+    <template v-if="false">
+      <Separator
+        class="mt-6 mb-2"
+        :label="t('labels.or').toUpperCase()"
+      />
+
+      <footer>
+        <Button variant="link">
+          {{ t("portal.joinAdminPanel") }}
+        </Button>
+      </footer>
+    </template>
   </div>
 </template>
