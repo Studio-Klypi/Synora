b57ad0d5262e8ae14d43c3615747521ec17af7a2
loicmaes <production@maesloic.fr>
2025-02-27 23:40:55 +0100

    :rocket: Setup sidebar inside the app layout

--- layouts/app/default.vue
+++ layouts/app/default.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-
+import ClientSidebar from "~/components/library/sidebars/ClientSidebar.vue";
 </script>
 
 <template>
   <div role="application">
-    <!-- TODO: sidebar -->
+    <SidebarProvider>
+      <ClientSidebar />
 
-    <NuxtPage />
+      <SidebarInset>
+        <header class="flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <SidebarTrigger />
+          <p>breadcrumb</p>
+        </header>
+
+        <main class="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <NuxtPage />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   </div>
 </template>
--- pages/app/[companyUuid]/index.vue
+++ pages/app/[companyUuid]/index.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import type { IBackCompany } from "~/types/companies/companies";
+
 definePageMeta({
   layout: "app-default",
   authRequired: true,
 });
 
+const { t } = useI18n();
+
 const store = useCompaniesStore();
-const company = computed(() => store.selectedCompany);
+const company = computed(() => store.selectedCompany as IBackCompany);
+
+useHead({
+  title: t("tabs.company", {
+    page: t("overview.tab"),
+    name: company.value.name,
+  }),
+});
 </script>
 
 <template>
