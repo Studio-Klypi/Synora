5c9a3ba77e307b70ca02f6e86f7f35a8285d983f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-03-19 08:51:51 +0100

    :rocket: Setup default setting page

---
+++ pages/app/[companyUuid]/settings/presentation.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "app-default",
+  authRequired: true,
+});
+</script>
+
+<template>
+  <div role="main">
+    Coming Soon...
+  </div>
+</template>
