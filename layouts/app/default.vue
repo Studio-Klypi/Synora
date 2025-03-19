d5309c1d06e13fed14d883f9c8c85d2451721dea
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-03-19 08:52:52 +0100

    :lipstick: Migrate overflow property to component itself

--- layouts/app/default.vue
+++ layouts/app/default.vue
@@ -5,7 +5,7 @@ import ClientSidebar from "~/components/library/sidebars/ClientSidebar.vue";
 <template>
   <SidebarProvider>
     <ClientSidebar />
-    <SidebarInset class="overflow-hidden">
+    <SidebarInset>
       <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div class="flex items-center gap-2 px-4">
           <SidebarTrigger class="-ml-1" />
