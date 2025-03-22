05f6c57f596ca5c71f4fb5dcf26f6140a1b81f80
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-03-22 14:39:55 +0100

    :rocket: Add company members permissions

--- types/security/permissions.ts
+++ types/security/permissions.ts
@@ -6,9 +6,15 @@ export const CompanyPermissions = [
   "company.security.delete",
   "company.edit.general",
 ] as const;
+export const MembersPermissions = [
+  "members.add",
+  "members.edit-role",
+  "members.delete",
+] as const;
 export const Permissions = [
   ...RolePermissions,
   ...CompanyPermissions,
+  ...MembersPermissions,
 ] as const;
 
 export type EPermission = typeof Permissions[number];
