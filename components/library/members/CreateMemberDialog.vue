ba18e5806c50bdba0df0c700b19548d43eacfcdc
loicmaes <production@maesloic.fr>
2025-03-22 15:54:24 +0100

    :rocket: Add ability to create a member

--- components/library/members/CreateMemberDialog.vue
+++ components/library/members/CreateMemberDialog.vue
@@ -1,28 +1,63 @@
 <script setup lang="ts">
-import { X, UserPlus } from "lucide-vue-next";
+import { X, UserPlus, ChevronsUpDown, Check, LoaderCircle } from "lucide-vue-next";
 import { toTypedSchema } from "@vee-validate/zod";
 import * as z from "zod";
 import { useForm } from "vee-validate";
+import debounce from "debounce";
+import type { IBackUser } from "~/types/auth/users";
+import { cn } from "~/lib/utils";
+import type { IOpListResult } from "~/types/generics/database";
+import type { IBackCompany } from "~/types/companies/companies";
 
 const { t } = useI18n();
+const open = ref<boolean>(false);
 
 const store = useCompaniesStore();
+const company = computed(() => store.selectedCompany as IBackCompany);
 const roles = computed(() => store.getRoles);
 
 const schema = toTypedSchema(z.object({
-  email: z.string({
+  userUuid: z.string({
     message: t("errors.fields.required"),
-  }).email(t("errors.fields.email")),
+  }).min(1),
   role: z.number().optional(),
 }));
 const form = useForm({
   validationSchema: schema,
 });
-const submit = form.handleSubmit(async values => console.log(values));
+const submit = form.handleSubmit(async ({ userUuid, role }) => {
+  await store.createMember({
+    userUuid,
+    roleId: role,
+  });
+  open.value = false;
+});
+
+const fetchingUsers = ref<boolean>(false);
+const users = ref<IBackUser[]>([]);
+const debouncedSearch = debounce(fetchUsers, 100);
+async function fetchUsers(email: string) {
+  fetchingUsers.value = true;
+
+  try {
+    const response = await $fetch<IOpListResult<IBackUser>>(`/api/users/public-operations/search?email=${email}&company=${company.value.uuid}`);
+    users.value = response.data;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  catch (e) {
+    users.value = [];
+  }
+  finally {
+    fetchingUsers.value = false;
+  }
+}
 </script>
 
 <template>
-  <Dialog>
+  <Dialog
+    :open="open"
+    @update:open="open = $event"
+  >
     <DialogTrigger as-child>
       <slot />
     </DialogTrigger>
@@ -35,18 +70,57 @@ const submit = form.handleSubmit(async values => console.log(values));
         class="flex flex-col gap-4"
         @submit="submit"
       >
-        <FormField
-          v-slot="{ componentField }"
-          name="email"
-        >
-          <FormItem>
+        <FormField name="userUuid">
+          <FormItem class="flex flex-col">
             <FormLabel>{{ t("members.dialogs.create-member.fields.email") }}</FormLabel>
-            <FormControl v-bind="componentField">
-              <Input
-                type="email"
-                placeholder="john.doe@example.xyz"
-              />
-            </FormControl>
+
+            <Combobox
+              by="email"
+            >
+              <FormControl class="w-full">
+                <ComboboxAnchor>
+                  <div class="relative w-full items-center">
+                    <ComboboxInput
+                      :display-value="(val) => val?.email ?? ''"
+                      placeholder="john.doe@example.xyz"
+                      @input="debouncedSearch($event.target.value)"
+                    />
+                    <ComboboxTrigger class="absolute end-0 inset-y-0 flex items-center justify-center px-3">
+                      <ChevronsUpDown class="size-4 text-muted-foreground" />
+                    </ComboboxTrigger>
+                  </div>
+                </ComboboxAnchor>
+              </FormControl>
+
+              <ComboboxList align="start">
+                <ComboboxEmpty>
+                  <template v-if="fetchingUsers">
+                    <LoaderCircle class="animate-spin" />
+                  </template>
+                  <template v-else>
+                    {{ t("members.dialogs.create-member.errors.no-users") }}
+                  </template>
+                </ComboboxEmpty>
+
+                <ComboboxGroup>
+                  <ComboboxItem
+                    v-for="user in users"
+                    :key="user.email"
+                    :value="user"
+                    @select="() => {
+                      form.setFieldValue('userUuid', user.uuid)
+                    }"
+                  >
+                    <span>{{ user.firstName }} <span class="text-muted-foreground italic">({{ user.email }})</span></span>
+
+                    <ComboboxItemIndicator>
+                      <Check :class="cn('ml-auto size-4')" />
+                    </ComboboxItemIndicator>
+                  </ComboboxItem>
+                </ComboboxGroup>
+              </ComboboxList>
+            </Combobox>
+
             <FormMessage />
           </FormItem>
         </FormField>
