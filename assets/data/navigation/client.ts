5f5df89c6df77eac1dcdf36a6192f5f298e0b5c0
loicmaes <production@maesloic.fr>
2025-03-23 16:54:28 +0100

    :rocket: Setup featured routes (front-end side)

--- assets/data/navigation/client.ts
+++ assets/data/navigation/client.ts
@@ -1,4 +1,4 @@
-import { Book, Home, Settings, Tags, Users } from "lucide-vue-next";
+import { Landmark, Library, BookOpenText, ArrowLeftRight, Waypoints, Book, Home, Settings, Tags, Users } from "lucide-vue-next";
 import type { INavSection } from "~/types/frontend/navigation";
 
 export const navSections: INavSection[] = [
@@ -9,6 +9,62 @@ export const navSections: INavSection[] = [
         label: "overview",
         icon: Home,
         url: "/",
+      },
+    ],
+  },
+  {
+    label: "accounting",
+    children: [
+      {
+        type: "group",
+        label: "plan",
+        icon: Waypoints,
+        planned: true,
+        children: [
+          {
+            type: "item",
+            label: "accounts",
+            url: "/accounting/plan/accounts",
+          },
+          {
+            type: "item",
+            label: "journals",
+            url: "/accounting/plan/journals",
+          },
+        ],
+      },
+      {
+        type: "item",
+        label: "transactions",
+        icon: ArrowLeftRight,
+        planned: true,
+        url: "/accounting/transactions",
+      },
+      {
+        type: "item",
+        label: "exercise",
+        icon: BookOpenText,
+        planned: true,
+        url: "/accounting/exercises",
+      },
+      {
+        type: "group",
+        label: "documents",
+        icon: Library,
+        children: [
+          {
+            type: "item",
+            label: "results",
+            url: "/accounting/documents/results",
+            planned: true,
+          },
+          {
+            type: "item",
+            label: "review",
+            url: "/accounting/documents/review",
+            planned: true,
+          },
+        ],
       },
     ],
   },
@@ -53,6 +109,36 @@ export const navSections: INavSection[] = [
         label: "wiki",
         icon: Book,
         url: "https://wiki.synora.org/getting-started",
+      },
+    ],
+  },
+  {
+    label: "archives",
+    children: [
+      {
+        type: "group",
+        label: "accounting",
+        icon: Landmark,
+        children: [
+          {
+            type: "item",
+            label: "accounts",
+            url: "/archives/accounting/accounts",
+            planned: true,
+          },
+          {
+            type: "item",
+            label: "exercises",
+            url: "/archives/accounting/exercises",
+            planned: true,
+          },
+          {
+            type: "item",
+            label: "documents",
+            url: "/archives/accounting/documents",
+            planned: true,
+          },
+        ],
       },
     ],
   },
